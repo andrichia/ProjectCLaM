1f1901a53c8b79418b206e9dd7545ba986768375
Austyn <austyneugenewong@gmail.com>
2020-05-29 14:31:12 +0700

    Update and Delete report added

--- app/src/main/java/id/ac/umn/projectclam/DeleteActivity.java
+++ app/src/main/java/id/ac/umn/projectclam/DeleteActivity.java
@@ -1,14 +1,61 @@
 package id.ac.umn.projectclam;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DeleteActivity extends AppCompatActivity {
 
+    private static final String TAG = "DeleteActivity";
+    private static final int REQUEST_CODE = 101;
+    private static FirebaseFirestore db;
+    private static AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete);
+        db = FirebaseFirestore.getInstance();
+        Context mContext = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setCancelable(true);
+        builder.setTitle("Title");
+        builder.setMessage("Message");
+        builder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        dialog = builder.create();
+
+    }
+
+    public void delete(View v){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        dialog.show();
+        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        assert telephonyManager != null;
+        String phone = telephonyManager.getDeviceId();
+        db.collection("report").document(phone).delete();
     }
 }
